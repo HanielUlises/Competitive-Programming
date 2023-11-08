b7b711c1104fd226bb5d7a86faf2410a2275931a
Haniel Ulises <hanielulises2003@gmail.com>
2023-11-07 21:41:18 -0600

    Print sub sets

---
+++ Arrays/printSubArray.cpp
@@ -0,0 +1,20 @@
+#include "bits/stdc++.h"
+
+// All the sub sets of a given set
+void printSubArrays(int array, int size){
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            for(int k = i; k <= j, k++){
+                std::cout<< array[i]<<",";
+            }
+            std::cout<<std::endl;
+        }
+    }
+}
+
+int main (){
+    int array [] = {10,20,30,40,50,60};
+    int size = sizeof(array)/sizeof(int);
+
+    printSubArrays(array, size);
+}
