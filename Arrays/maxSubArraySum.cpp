a1783f49dd05e217d58435592bfd6d8bbf6e5ccc
Haniel Ulises <hanielulises2003@gmail.com>
2023-11-14 19:46:07 -0600

    Maximum subArray sum

---
+++ Arrays/maxSubArraySum.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int maxSumSubarray(vector<int> A) {
+    int maxSoFar = INT_MIN, maxEndingHere = 0;
+
+    for (int i = 0; i < A.size(); i++) {
+        maxEndingHere = maxEndingHere + A[i];
+        if (maxSoFar < maxEndingHere)
+            maxSoFar = maxEndingHere;
+        if (maxEndingHere < 0)
+            maxEndingHere = 0;
+    }
+    return maxSoFar;
+}
+
+int main() {
+    vector<int> A = {-2, -3, 4, -1, -2, 1, 5, -3};
+    cout << "Maximum sum of a subarray is " << maxSumSubarray(A) << endl;
+    return 0;
+}
