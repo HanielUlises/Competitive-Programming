49e19ef4b572f1456f20b6eb02e57e474fa69c38
Haniel Ulises <hanielulises2003@gmail.com>
2023-11-20 20:50:00 -0600

    Insertion sort

--- Basic data structures & algorithms/Sorting Algorithms/insertionSort.cpp
+++ Basic data structures & algorithms/Sorting Algorithms/insertionSort.cpp
@@ -2,7 +2,16 @@
 
 std::vector<int> insertionSort (std::vector<int> v){
     size_t size = v.size();
-    for (int i = 1;)
+    for (int i = 1; i <= size - 1; i++){
+        int actual = v[i];
+        int prev = i - 1;
+
+        while(prev >= 0 && v[prev] > actual){
+            v[prev + 1] = v[prev];
+            prev = prev - 1;
+        }
+        v[prev + 1] = actual;
+    }
     return v;
 }
 
