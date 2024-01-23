04e3902b9d373303cf98d1405a4bc144bd847a7e
Haniel Ulises <hanielulises2003@gmail.com>
2024-01-22 21:36:35 -0600

    Wave Print matrix

---
+++ Basic data structures & algorithms/Matrices/wavePrint.cpp
@@ -0,0 +1,31 @@
+#include "bits/stdc++.h"
+
+vector<int> WavePrint(int m, int n, vector<vector<int>> arr) {
+    vector<int> result;
+    for (int col = n - 1; col >= 0; col--) {
+        if (col % 2 == 0) {
+            for (int row = 0; row < m; row++) {
+                result.push_back(arr[row][col]);
+            }
+        } else {
+            for (int row = m - 1; row >= 0; row--) {
+                result.push_back(arr[row][col]);
+            }
+        }
+    }
+    return result;
+}
+
+int main (){
+    int arr[100][100];
+
+    int n,m;
+
+    std::cin>>n>>m;
+
+    for(int i = 0; i < n; i ++){
+        for(int j = 0; j < m; j++){
+            std::cin>>arr[i][j];
+        }
+    }
+}
