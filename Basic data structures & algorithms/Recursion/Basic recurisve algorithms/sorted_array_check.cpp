e2b894541c6df4f6710335338a80e537d54072ad
Haniel Ulises <88525113+HanielUlises@users.noreply.github.com>
2024-04-10 22:52:46 -0600

    Recursive sorted array verifier

---
+++ Basic data structures & algorithms/Recursion/Basic recurisve algorithms/sorted_array_check.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+bool is_sorted(int arr[], int n){
+    if (n == 1 or n == 0){
+        return true;
+    }
+
+    if(arr[0]<arr[1] and is_sorted(arr + 1,n-1)){
+        return true;
+    }
+
+    return false;
+}
+
+int main (){
+    int arr[]  = {1,2,3,4,5,6};
+    int n = sizeof(arr)/sizeof(int);
+
+    std::cout << is_sorted(arr, n);
+}
