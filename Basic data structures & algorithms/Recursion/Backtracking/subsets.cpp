2cf4588cd6f786757600a733cca8029d4c407b4f
Haniel Ulises <88525113+HanielUlises@users.noreply.github.com>
2024-04-11 20:06:44 -0600

    Subsets

---
+++ Basic data structures & algorithms/Recursion/Backtracking/subsets.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+
+void generateSubsets(int index, const std::string &input, std::string &current, std::vector<std::string> &result) {
+    // Base case: if the end of the string is reached, add the current subset to the result
+    if (index == input.length()) {
+        result.push_back(current);
+        return;
+    }
+
+    // Current character
+    current.push_back(input[index]);
+    generateSubsets(index + 1, input, current, result);
+
+    // Exclude the current character (backtrack)
+    current.pop_back();
+    generateSubsets(index + 1, input, current, result);
+}
+
+int main() {
+    std::string input;
+    std::cin >> input; 
+
+    std::vector<std::string> subsets;
+    std::string currentSubset;
+
+    generateSubsets(0, input, currentSubset, subsets);
+
+    for (const std::string &subset : subsets) {
+        if (subset.empty()) {
+            std::cout << "{}\n";
+        } else {
+            std::cout << subset << '\n';
+        }
+    }
+
+    return 0;
+}
